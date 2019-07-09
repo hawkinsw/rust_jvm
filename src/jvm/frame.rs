0af0f275fd9fad475c049a6fc77d16798a1a9f8e
Will Hawkins <whh8b@obs.cr>
2019-07-09 01:35:58 -0400

    Adding frames that the methodarea can use.

--- src/jvm/frame.rs
+++ src/jvm/frame.rs
@@ -1,3 +1,27 @@
+use jvm::typevalues::JvmTypeValue;
+use std::fmt;
+
 #[derive(Clone,Default)]
-pub struct Frame{
+pub struct Frame<'a> {
+	pub operand_stack: Vec<JvmTypeValue<'a>>,
+}
+
+impl<'a> Frame<'a> {
+	pub fn new() -> Self {
+		Frame{operand_stack: Vec::<JvmTypeValue<'a>>::new()}
+	}
+}
+
+impl<'a> fmt::Display for Frame<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let mut result: fmt::Result;
+		result = write!(f,"");
+		for entry in &self.operand_stack {
+			match entry {
+				JvmTypeValue::Primitive(primitive) => result = write!(f, "Primitive: {}", primitive),
+				JvmTypeValue::Reference(reference) => result = write!(f, "Reference: PASS" ),
+			}
+		}
+		result
+	}
 }
---
+++ src/jvm/typevalues.rs
@@ -0,0 +1,62 @@
+use std::fmt;
+
+#[derive(Clone)]
+pub enum JvmPrimitiveType {
+	Boolean,
+	Integer,
+}
+
+#[derive(Clone)]
+pub enum JvmReferenceType<'a> {
+	Array(&'a JvmTypeValue<'a>, u64),
+	Class(String),
+	Interface(String),
+}
+
+#[derive(Clone)]
+pub struct JvmPrimitiveTypeValue {
+	tipe: JvmPrimitiveType,
+	value: u64,
+}
+
+impl JvmPrimitiveTypeValue {
+	pub fn new(tipe: JvmPrimitiveType, value: u64) -> Self {
+		Self{tipe: tipe, value: value}
+	}
+}
+
+#[derive(Clone)]
+pub struct JvmReferenceTypeValue<'a> {
+	tipe: JvmReferenceType<'a>,
+	reference: u64,
+}
+
+impl<'a> JvmReferenceTypeValue<'a> {
+	pub fn new_array(dimension: u64, component_type: &'a JvmTypeValue<'a>, reference: u64) -> Self {
+		JvmReferenceTypeValue{tipe: JvmReferenceType::Array(component_type, dimension), reference: reference}
+	}
+
+	pub fn new_class(name: String, reference: u64) -> Self {
+		JvmReferenceTypeValue{tipe: JvmReferenceType::Class(name), reference: reference}
+	}
+
+	pub fn new_interface(name: String, reference: u64) -> Self {
+		JvmReferenceTypeValue{tipe: JvmReferenceType::Interface(name), reference: reference}
+	}
+}
+
+impl fmt::Display for JvmPrimitiveTypeValue {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let type_name = match self.tipe {
+			JvmPrimitiveType::Boolean => "Boolean",
+			JvmPrimitiveType::Integer => "Boolean",
+		};
+		write!(f, "{}: {}", type_name, self.value)
+	}
+}
+
+#[derive(Clone)]
+pub enum JvmTypeValue<'a> {
+	Primitive(JvmPrimitiveTypeValue),
+	Reference(JvmReferenceTypeValue<'a>),
+}
