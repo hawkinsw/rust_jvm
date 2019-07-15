b0b137a64f7f6fe13ffce95fa2eb5b8dcf143604
Will Hawkins <whh8b@obs.cr>
2019-07-14 23:51:34 -0400

    Flesh out invokestatic implementation.

--- src/jvm/jvmthread.rs
+++ src/jvm/jvmthread.rs
@@ -6,6 +6,8 @@ use jvm::typevalues::JvmPrimitiveTypeValue;
 use jvm::typevalues::JvmPrimitiveType;
 use enum_primitive::FromPrimitive;
 use jvm::opcodes::OperandCodes;
+use jvm::constant::Constant;
+use std::rc::Rc;
 
 pub struct JvmThread {
 	debug: bool,
@@ -31,13 +33,14 @@ impl JvmThread {
 			if self.debug {
 				println!("Loaded class {}.\n", class);
 			}
-			if let Some(method) = class.get_method(method_name) {
+			if let Some(method) = class.get_method_ref_by_name(method_name) {
 				let mut frame = Frame::new();
+				frame.constant_pool = Some(class.get_constant_pool_ref());
+				frame.class = Some(&class);
 				/*
 				 * Load up the frame's stack with the CLI arguments.
 				 */
 				frame.operand_stack.push(JvmTypeValue::Primitive(JvmPrimitiveTypeValue::new(JvmPrimitiveType::Boolean, 0)));
-				frame.constant_pool = Some(class.get_constant_pool());
 
 				if self.debug {
 					println!("Frame: {}", frame);
@@ -49,26 +52,25 @@ impl JvmThread {
 		false
 	}
 
-	fn execute_method<'b>(&mut self, method: &Method, frame: Frame<'b>) -> bool {
-		if let Some(code)=method.get_code_attribute(frame.constant_pool.unwrap()) {
-			if self.debug {
-				println!("Method's code attribute:\n{}", code);
-			}
-			self.pc = code.code_offset + 0;
-			let mut pc_incr = self.execute_opcode(&code.bytes[self.pc ..], &frame);
+	fn execute_method(&mut self, method: &Method, frame: Frame) -> bool {
+		if let Some(code)=method.get_code(frame.constant_pool.unwrap()) {
+			let mut pc = 0;
+			let mut pc_incr = self.execute_opcode(&code[pc ..], &frame);
 			while pc_incr != 0 {
 				if self.debug {
 					print!("Doing next opcode\n");
 				}
-				self.pc += pc_incr;
-				pc_incr = self.execute_opcode(&code.bytes[self.pc ..], &frame);
+				pc += pc_incr;
+				pc_incr = self.execute_opcode(&code[pc ..], &frame);
 			}
 		}
 		return true
 	}
 
-	fn execute_opcode(&self, bytes: &[u8], frame: &Frame) -> usize {
+	fn execute_opcode(&mut self, bytes: &[u8], frame: &Frame) -> usize {
 		let mut pc_incr: usize;
+		let constant_pool = frame.constant_pool.unwrap();
+		let class = frame.class.unwrap();
 
 		let opcode = bytes[0];
 		if self.debug {
@@ -79,12 +81,7 @@ impl JvmThread {
 				if self.debug {
 					print!("invokestatic\n");
 				}
-				let method_index: u16 = ((bytes[1] as u16)<<8)|(bytes[2] as u16);
-				print!("method_index: {:x}\n", method_index);
-				let constant = frame.constant_pool.unwrap().get(method_index as usize);
-				if self.debug {
-					println!("constant: {}", constant);
-				}
+				self.execute_invokestatic(bytes, frame);
 				pc_incr = 3;
 			},
 			Some(OperandCodes::OPCODE_pop) => {
@@ -98,5 +95,56 @@ impl JvmThread {
 			}
 		}
 		pc_incr
+	}
+
+	pub fn execute_invokestatic(&mut self, bytes: &[u8], frame: &Frame) {
+		let constant_pool = frame.constant_pool.unwrap();
+		let class = frame.class.unwrap();
+		let method_index = (((bytes[1] as u16)<<8)|(bytes[2] as u16)) as usize;
+
+		match constant_pool.get_constant_ref(method_index) {
+			Constant::Methodref(_, class_index, method_index,) => {
+				if let Constant::Class(_, class_name_index) = constant_pool.get_constant_ref(*class_index as usize) {
+					if let Constant::NameAndType(_, method_name_index, _) = constant_pool.get_constant_ref(*method_index as usize) {
+						if let Constant::Utf8(_, _, _, class_name) = constant_pool.get_constant_ref(*class_name_index as usize) {
+							if let Constant::Utf8(_, _, _, method_name) = constant_pool.get_constant_ref(*method_name_index as usize) {
+								if self.debug {
+									println!("Invoke Static: {}.{}", class_name, method_name);
+								}
+								/*
+								 * Steps:
+								 * 1. Get the class containing the method.
+								 * 2. Get the method.
+								 * 3. Create a frame.
+								 * 4. Populate the frame.
+								 * 5. Execute the method
+								 */
+								if !self.methodarea.is_class_loaded(&class_name) {
+									println!("We do need to load the class!");
+									/*
+									 * TODO
+									 */
+								} 
+
+								if let Some(invoked_class) = self.methodarea.get_class_rc(&class_name) {
+									if let Some(method) = invoked_class.get_methods_ref().get_by_name(method_name, invoked_class.get_constant_pool_ref()) {
+										if self.debug {
+											println!("method: {}", method);
+										}
+										let mut frame = Frame::new();
+										frame.constant_pool = Some(invoked_class.get_constant_pool_ref());
+										frame.class = Some(&invoked_class);
+										self.execute_method(&method, frame);
+									}
+								} else {
+									println!("Error: Could not execute static method {}.{}", class_name, method_name);
+								}
+							}
+						}
+					}
+				}
+			},
+			_ => ()
+		};
 	}
 }
