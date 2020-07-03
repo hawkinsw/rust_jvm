a2051424d591eb828d656159d4040d5c93323f0d
Will Hawkins <hawkinsw@obs.cr>
2020-07-03 00:26:23 -0400

    Add a Test file for exceptions.

---
+++ java_test/Exceptional.java
@@ -0,0 +1,19 @@
+public class Exceptional {
+    public static void exceptionalMethod() throws Exception {
+        throw new Exception();
+    }
+    public static void unExceptionalMethod() {
+    }
+
+    public static void main(String args[]) {
+        int result = 0;
+        try {
+            exceptionalMethod();
+            //unExceptionalMethod();
+            result = 1;
+        } catch (Exception e) {
+            System.out.println("This is exceptional!");
+            result = -1;
+        }
+    }
+}
